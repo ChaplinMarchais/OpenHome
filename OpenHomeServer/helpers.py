4ff017fa6f2eab1df7808c5cf67f98d15f8a1a4f
Chaplin Marchais <chap.m.gigga@gmail.com>
2025-05-10 18:20:26 -0700

    Create helper for querying the database and include in .gitignore for all .db files

--- OpenHomeServer/helpers.py
+++ OpenHomeServer/helpers.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import redirect, render_template, session
 from functools import wraps
 
@@ -17,3 +19,29 @@ def login_required(f):
         return f(*args, **kwargs)
     
     return decorated_function
+
+def query(db_name, query, params=None):
+    """
+    Query the database for a given SQL statement with the provided optional arguements.
+
+    Args:
+        db_name (string): Connection string for the db
+        query (string): SQL query to execute
+        params (string, optional): Optional params to pass to the SQL statement. Defaults to None.
+
+    Returns:
+        Object: Result of query
+    """
+    try:
+        with sqlite3.connect(db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params or ())
+            
+            if query.lower().startswith("select"):
+                return cursor.fetchall()
+            else:
+                 conn.commit()
+                 return None
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return None
